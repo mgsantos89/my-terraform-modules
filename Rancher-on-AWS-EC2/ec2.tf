537395f91e267878a98c2897742722e7c8e384c0
Marco Gomes <marco@mgsantos.com.br>
2024-06-26 15:59:49 -0300

    Correção de AMI

--- Rancher-on-AWS-EC2/ec2.tf
+++ Rancher-on-AWS-EC2/ec2.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "rancher-server" {
+data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
@@ -14,8 +14,8 @@ data "aws_ami" "rancher-server" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+resource "aws_instance" "rancher-ec2" {
+  ami           = data.aws_ami.ubutu.id
   instance_type = "t3.medium"
   subnet_id= var.subnet_id
   tags = {
