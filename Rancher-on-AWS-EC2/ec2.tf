835bf63bd2a2f6111a2def7c0f859ea9544beb7b
Marco Gomes <marco@mgsantos.com.br>
2024-06-26 15:49:30 -0300

    Add Subnet Config

--- Rancher-on-AWS-EC2/ec2.tf
+++ Rancher-on-AWS-EC2/ec2.tf
@@ -17,7 +17,7 @@ data "aws_ami" "rancher-server" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.medium"
-
+  subnet_id= var.subnet_id
   tags = {
     Name = "rancher-server"
   }
